53cc808133d122889384ea715376ea7f4088616c
JosianeUwineza <75666755+JosianeUwineza@users.noreply.github.com>
2026-02-16 17:36:55 +0200

    Add files via upload

---
+++ introduce_NLP/learn_nltk_tokenization_nlp.py
@@ -0,0 +1,17 @@
+#import nltk
+# nltk.download('punkt')
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+example_string = """ Muad'Dib learned rapidly because his first training was in how to learn.
+And the first lesson of all was the basic trust that he could learn.
+It's shocking to find how many people do not believe they can learn,
+and how many more believe learning to be difficult."""
+
+tokenized_sentence_ex_string = sent_tokenize(example_string)
+print('- --------------------------------------------------------------------------------------')
+print(tokenized_sentence_ex_string)
+print('------------------------------------------------------------------------------------------')
+tokenized_word_ex_string = word_tokenize(example_string)
+
+print(tokenized_word_ex_string)
